fd85c835d8f9440bc559b069445e043670723610
Chris Moody <cmoody@transcon.com>
2015-05-13 15:37:27 -0700

    refactor module pull logic into sub class

--- lib/fuzzy_record/fuzzy_searcher.rb
+++ lib/fuzzy_record/fuzzy_searcher.rb
@@ -6,14 +6,14 @@ class FuzzyRecord::FuzzySearcher
     @search = create_search
   end
   def find
-    @class.all.select{ |record| include_record(search,record)}.sort_by{|record| sorter(search,record)}
+    @class.all.select{ |record| include_record(record)}.sort_by{|record| sorter(record)}
   end
   private
   def generalize(str) ".*#{str.gsub(/[^a-zA-Z&0-9]/, "").chars.to_a.join(".*")}.*" end
   def ave(arry) arry.sum.to_f / arry.length end
   def matches?(record,k,v) record.send(k) =~ /#{generalize(v)}/i end
-  def sorter(search,record) ave(search.map{|k,v| record.send(k) ^ v}) end
-  def include_record(search,record) search.map{|k,v| matches?(record,k,v)}.inject(:|) end
+  def sorter(record) ave(@search.map{|k,v| record.send(k) ^ v}) end
+  def include_record(record) @search.map{|k,v| matches?(record,k,v)}.inject(:|) end
 
   def create_search
     return @args if @args.is_a? Hash
